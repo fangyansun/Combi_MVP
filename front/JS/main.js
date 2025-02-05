fc168731fa78174031dbe6880deb703ea848ec3b
Philanoe <50548547+Philanoe@users.noreply.github.com>
2025-02-05 06:07:08 +0100

    add a namespace with an access to temperatures spans

--- front/JS/main.js
+++ front/JS/main.js
@@ -1,1 +1,14 @@
-console.log("hello the world")
+"use script"
+
+let main_namespace = {
+
+    temperature_1 : document.getElementById("temperature_1"),
+    temperature_2 : document.getElementById("temperature_2"),
+    temperature_3 : document.getElementById("temperature_3"),
+    temperature_4 : document.getElementById("temperature_4")
+}
+
+console.log("hello")
+
+
+
